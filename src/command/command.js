b7cde56a4e0b5816bf89e5b041451d8baa2baa8b
KenethSandoval <kenethra.74@gmail.com>
2021-07-26 01:28:47 -0600

    feat: refactor 1

---
+++ src/command/command.js
@@ -0,0 +1,11 @@
+const commands = [
+    {command: 'chiste'},
+    {command: 'help'},
+    {command: 'command'},
+    {command: 'nipedo'},
+    {command: 'hora'},
+    {command: 'avatar'},
+    {command: 'senosfu}ekeneth'}
+];
+
+module.exports = commands;
